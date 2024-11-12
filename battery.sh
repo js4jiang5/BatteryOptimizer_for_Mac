4bf2c15b8c721369b428d4ac86b633b0d8b73759
jsjiang <js4jiang5@users.noreply.github.com>
2024-11-12 21:59:19 +0800

    v0.0.20

--- battery.sh
+++ battery.sh
@@ -2911,29 +2911,59 @@ if [[ "$action"  == "test_intel_discharge" ]]; then
 
 	sudo smc -k BSAC -w 02; echo "set BSAC = 02"
 	sudo smc -k CH0K -w 00; echo "set CH0K = 00"
-	sleep 1
+	sudo smc -k CH0B -w 21; echo "set CH0B = 21"
+	sleep 5
+	b0ac=$(read_smc B0AC); echo "B0AC = $b0ac"
 	bsac=$(read_smc BSAC); echo "BSAC = $bsac"
 	acen=$(read_smc ACEN); echo "ACEN = $acen"
 	ch0h=$(read_smc CH0H); echo "CH0H = $ch0h"
 	ch0k=$(read_smc CH0K); echo "CH0K = $ch0k"
 	ch0b=$(read_smc CH0B); echo "CH0B = $ch0b"
-	for i in {0..255}; do
-		i_hex=$(printf "%02x" $i)
-		sudo smc -k CH0B -w $i_hex; echo "set CH0B = $i_hex"
-		sleep 5
-		b0ac=$(read_smc B0AC); echo "B0AC = $b0ac"
-		chbi=$(read_smc CHBI); echo "CHBI = $chbi"
-		if [[ $((0x${b0ac})) -gt 0 ]]; then
-			echo "found"
-			sudo smc -k CH0B -w 00; echo "set CH0B = 00"
-			sleep 1
-			acen=$(read_smc ACEN); echo "ACEN = $acen"
-			bsac=$(read_smc BSAC); echo "BSAC = $bsac"
-			ch0h=$(read_smc CH0H); echo "CH0H = $ch0h"
-			ch0k=$(read_smc CH0K); echo "CH0K = $ch0k"
-			#break;
-		fi
-	done
+	chbi=$(read_smc CHBI); echo "CHBI = $chbi"
+	sudo smc -k CH0B -w 00; echo "set CH0B = 00"
+	sleep 5
+	b0ac=$(read_smc B0AC); echo "B0AC = $b0ac"
+
+	sudo smc -k BSAC -w 22; echo "set BSAC = 22"
+	sudo smc -k CH0K -w 00; echo "set CH0K = 00"
+	sudo smc -k CH0B -w 21; echo "set CH0B = 21"
+	sleep 5
+	b0ac=$(read_smc B0AC); echo "B0AC = $b0ac"
+	bsac=$(read_smc BSAC); echo "BSAC = $bsac"
+	acen=$(read_smc ACEN); echo "ACEN = $acen"
+	ch0h=$(read_smc CH0H); echo "CH0H = $ch0h"
+	ch0k=$(read_smc CH0K); echo "CH0K = $ch0k"
+	ch0b=$(read_smc CH0B); echo "CH0B = $ch0b"
+	chbi=$(read_smc CHBI); echo "CHBI = $chbi"
+	sudo smc -k CH0B -w 00; echo "set CH0B = 00"
+	sleep 5
+	b0ac=$(read_smc B0AC); echo "B0AC = $b0ac"
+	
+	#sudo smc -k BSAC -w 02; echo "set BSAC = 02"
+	#sudo smc -k CH0K -w 00; echo "set CH0K = 00"
+	#sleep 1
+	#bsac=$(read_smc BSAC); echo "BSAC = $bsac"
+	#acen=$(read_smc ACEN); echo "ACEN = $acen"
+	#ch0h=$(read_smc CH0H); echo "CH0H = $ch0h"
+	#ch0k=$(read_smc CH0K); echo "CH0K = $ch0k"
+	#ch0b=$(read_smc CH0B); echo "CH0B = $ch0b"
+	#for i in {0..255}; do
+	#	i_hex=$(printf "%02x" $i)
+	#	sudo smc -k CH0B -w $i_hex; echo "set CH0B = $i_hex"
+	#	sleep 5
+	#	b0ac=$(read_smc B0AC); echo "B0AC = $b0ac"
+	#	chbi=$(read_smc CHBI); echo "CHBI = $chbi"
+	#	if [[ $((0x${b0ac})) -gt 0 ]]; then
+	#		echo "found"
+	#		sudo smc -k CH0B -w 00; echo "set CH0B = 00"
+	#		sleep 1
+	#		acen=$(read_smc ACEN); echo "ACEN = $acen"
+	#		bsac=$(read_smc BSAC); echo "BSAC = $bsac"
+	#		ch0h=$(read_smc CH0H); echo "CH0H = $ch0h"
+	#		ch0k=$(read_smc CH0K); echo "CH0K = $ch0k"
+	#		#break;
+	#	fi
+	#done
 
 	#sudo smc -k BSAC -w 22; echo "set BSAC = 22"
 	#sleep 1
